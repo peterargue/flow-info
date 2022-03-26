5e230e9ffa683548b564c989afddd1b027d846b3
Peter Argue <89119817+peterargue@users.noreply.github.com>
2022-03-26 16:14:31 -0700

    add download cli command

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # flow-info
 Tools for downloading flow network information
 
+See [examples/main.go](examples/main.go) for a usage example.
+
+You can also use the included command to download files for a specific spork via the cli.
+
 ### Usage
-The following command runs the example app that downloads the `node-info.json` and `root-protocol-state-snapshot.json` 
+The following command runs the download command that downloads the `node-infos.pub.json` and `root-protocol-state-snapshot.json`
 files for the `mainnet16` flow network:
 ```
-go run examples/main.go --spork-name mainnet16
+go run cmd/download/main.go \
+	--spork-name mainnet16 \
+	--root-protocol-state-snapshot bootstrap/public-root-information/root-protocol-state-snapshot.json \
+	--root-protocol-state-snapshot-sig bootstrap/public-root-information/root-protocol-state-snapshot.json.asc \
+	--node-info bootstrap/public-root-information/node-infos.pub.json
 ```
---
+++ cmd/download/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	info "github.com/peterargue/flow-info"
+)
+
+func main() {
+	var sporkName,
+		rootCheckpointFile,
+		rootProtocolStateSnapshot,
+		rootProtocolStateSnapshotSignature,
+		nodeInfo string
+
+	flag.StringVar(&sporkName, "spork-name", "", "spork name (e.g. mainnet16, testnet33, etc)")
+	flag.StringVar(&rootCheckpointFile, "root-checkpoint", "", "path where rootCheckpointFile will be written")
+	flag.StringVar(&rootProtocolStateSnapshot, "root-protocol-state-snapshot", "", "path where rootProtocolStateSnapshot will be written")
+	flag.StringVar(&rootProtocolStateSnapshotSignature, "root-protocol-state-snapshot-sig", "", "path where rootProtocolStateSnapshotSignature will be written")
+	flag.StringVar(&nodeInfo, "node-info", "", "path where nodeInfo will be written")
+	flag.Parse()
+
+	if sporkName == "" {
+		fmt.Println("Missing --spork-name")
+		flag.Usage()
+		return
+	}
+
+	if rootCheckpointFile == "" &&
+		rootProtocolStateSnapshot == "" &&
+		rootProtocolStateSnapshotSignature == "" &&
+		nodeInfo == "" {
+
+		fmt.Println("At least one of --root-checkpoint, --root-protocol-state-snapshot, --root-protocol-state-snapshot-sig, --node-info must be specified")
+		flag.Usage()
+		return
+	}
+
+	spork, err := info.LoadSporkJSON(sporkName)
+	if err != nil {
+		log.Fatalf("Error loading spork: %v", err)
+	}
+
+	if rootCheckpointFile != "" {
+		err = info.Download(spork.StateArtefacts.RootCheckpointFile, rootCheckpointFile)
+		if err != nil {
+			log.Fatalf("Error downloading root-checkpoint: %v", err)
+		}
+		log.Printf("wrote root-checkpoint to %s", rootCheckpointFile)
+	}
+
+	if rootProtocolStateSnapshot != "" {
+		err = info.Download(spork.StateArtefacts.RootProtocolStateSnapshot, rootProtocolStateSnapshot)
+		if err != nil {
+			log.Fatalf("Error downloading root-protocol-state-snapshot: %v", err)
+		}
+		log.Printf("wrote root-protocol-state-snapshot to %s", rootProtocolStateSnapshot)
+	}
+
+	if rootProtocolStateSnapshotSignature != "" {
+		err = info.Download(spork.StateArtefacts.RootProtocolStateSnapshotSignature, rootProtocolStateSnapshotSignature)
+		if err != nil {
+			log.Fatalf("Error downloading root-protocol-state-snapshot-sig: %v", err)
+		}
+		log.Printf("wrote root-protocol-state-snapshot-sig to %s", rootProtocolStateSnapshotSignature)
+	}
+
+	if nodeInfo != "" {
+		err = info.Download(spork.StateArtefacts.NodeInfo, nodeInfo)
+		if err != nil {
+			log.Fatalf("Error downloading node-info: %v", err)
+		}
+		log.Printf("wrote node-info to %s", nodeInfo)
+	}
+}
